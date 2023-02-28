45e9812156f7fe85578c958b312e52368fd907af
aiyin <372020407@qq.com>
2023-02-28 18:32:40 +0800

    init project

--- vue.config.js
+++ vue.config.js
@@ -2,11 +2,6 @@ const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
   lintOnSave:false,
-  pluginOptions:{
-      electronBuilder:{
-          nodeIntegration:true
-      }
-  }
 })
 module.exports = { configureWebpack: {
     resolve: {
